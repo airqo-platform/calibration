3a6b035b5e0a43eee390d42c4360b8eed4bd8b36
Michael Smith <m.t.smith@sheffield.ac.uk>
2020-10-06 09:36:48 +0100

    all working

--- calibration/__init__.py
+++ calibration/__init__.py
@@ -5,6 +5,8 @@ psd_kernels = tfp.math.psd_kernels
 import pandas as pd
 import numpy as np
 from tensorflow_probability import distributions as tfd
+from scipy.linalg import block_diag
+
 
 def placeinducingpoints(X,S,C,M=16):
     """
@@ -29,35 +31,100 @@ class Kernel():
     def matrix(self,X1,X2):    
         cov = self.gpflowkernel.K(X1,X2) * ((X2[:,1][None,:]==X1[:,1][:,None]) & (X2[:,2][None,:]==X1[:,2][:,None]))
         return cov.numpy().astype(np.float32)
-        
-        
-class EQ(Kernel):
-    def __init__(self,ls=20,v=0.5):
+
+class BrokenMultiKernel(): #delete this class
+    def __init__(self,gpflowkernels,indices):
         """
-        An EQ kernel built so that they return zero unless both the second
-        and third column are equal between the two inputs.
+        gpflowkernels = a list of GPflow kernels.
+          E.g. gpflowkernels = [gpflow.kernels.RBF(1.0,200),gpflow.kernels.Bias(1.0)]
+        indices = a list of lists,
+         each row refers to the one or more parameters.
+         each element of each list refers to a sensor.
+          E.g. indexes = [[0,0,0,0,1,1]]
         """
-        self.ls = ls
-        self.v = v
-    def k(self,x1,x2):
-        """
-        For two points, x1 and x2, computes their covariance for this kernel
-        """
-        return self.v*np.exp(-(x1-x2)**2/(2*self.ls**2))  #+0.5**2
-    
+        self.gpflowkernels = gpflowkernels
+        self.indices = indices
     def matrix(self,X1,X2):
-        """For locations defined by X1 and X2 compute covariance matrix"""    
-        try:
-            X1 = X1.numpy()
-            X2 = X2.numpy()
-        except AttributeError:
-            pass
-        r = np.zeros([X1.shape[0],X2.shape[0]])
-        for i1,x1 in enumerate(X1):
-            for i2,x2 in enumerate(X2):
-                    if (x1[1]==x2[1]) and (x1[2]==x2[2]): #if same sensor and component
-                        r[i1,i2] = self.k(x1[0],x2[0])
-        return tf.Variable(r,dtype=tf.float32)
+        blockks = []
+        for param,index in enumerate(self.indices):
+            for idx,i in enumerate(index):
+                blockks.append((self.gpflowkernels[i].K(X1[(X1[:,1]==idx) & (X1[:,2]==param)],X2[(X2[:,1]==idx) & (X2[:,2]==param)])).numpy())
+        return block_diag(*blockks).astype(np.float32)
+        
+        
+class TemporaryMultiKernel(): #delete this class
+    def __init__(self,gpflowkernels,indices):
+        self.gpflowkernel = gpflowkernels[0]
+    def matrix(self,X1,X2):    
+        cov = self.gpflowkernel.K(X1,X2) * ((X2[:,1][None,:]==X1[:,1][:,None]) & (X2[:,2][None,:]==X1[:,2][:,None]))
+        return cov.numpy().astype(np.float32)
+ 
+
+class MultiKernel():
+    def __init__(self,gpflowkernels,indices):
+        """
+        A kernel in which each sensor and parameter is independent
+        The covariance for each sensor and parameter is defined by the list of gpflow kernels.
+        Specify one or more gpflow kernels in a list, and index these using the indices
+        parameter.
+        
+        gpflowkernels = a list of GPflow kernels.
+          E.g. gpflowkernels = [gpflow.kernels.RBF(1.0,10),gpflow.kernels.RBF(1.0,200),gpflow.kernels.Bias(1.0)]
+        indices = a list of lists,
+         each row refers to the one or more parameters.
+         each element of each list refers to a sensor.
+          E.g. indexes = [[0,0,0,0,1,1],[2,2,2,2,2,2]]
+         in this example the first parameter for all the sensors are RBF kernels, for the first four sensors
+         the lengthscale is just 10, but for the last two it is 200.
+         The second parameter is modelled with a bias kernel for all the sensors.
+        """
+        self.gpflowkernels = gpflowkernels
+        self.indices = indices
+    def oldmatrix(self,X1,X2):    
+        cov = np.zeros([len(X1),len(X2)])
+        for index_param,indices_param in enumerate(self.indices): #loop over the parameters
+            for index_sensor,index in enumerate(indices_param): #loop over the sensors
+                #print(index_param,index_sensor,index)
+                #print(self.gpflowkernels)
+                cov+=self.gpflowkernels[index].K(X1,X2) * ((X2[:,1][None,:]==index_sensor) & (X1[:,1][:,None]==index_sensor) & (X2[:,2][None,:]==index_param) & (X1[:,2][:,None]==index_param))
+                print(cov.shape)      
+        return cov.numpy().astype(np.float32)
+    def matrix(self,X1,X2):    
+        cov = np.zeros([len(X1),len(X2)])
+        for index_param,indices_param in enumerate(self.indices): #loop over the parameters
+            for index_sensor,index in enumerate(indices_param): #loop over the sensors
+                keep1 = np.where((X1[:,1]==index_sensor) & (X1[:,2]==index_param))[0]
+                keep2 = np.where((X2[:,1]==index_sensor) & (X2[:,2]==index_param))[0]
+                cov[np.ix_(keep1,keep2)] = self.gpflowkernels[index].K(X1[keep1,:],X2[keep2,:])
+        return cov.astype(np.float32)
+        
+#class EQ(Kernel):
+#    def __init__(self,ls=20,v=0.5):
+#        """
+#        An EQ kernel built so that they return zero unless both the second
+#        and third column are equal between the two inputs.
+#        """
+#        self.ls = ls
+#        self.v = v
+#    def k(self,x1,x2):
+#        """
+#        For two points, x1 and x2, computes their covariance for this kernel
+#        """
+#        return self.v*np.exp(-(x1-x2)**2/(2*self.ls**2))  #+0.5**2
+#    
+#    def matrix(self,X1,X2):
+#        """For locations defined by X1 and X2 compute covariance matrix"""    
+#        try:
+#            X1 = X1.numpy()
+#            X2 = X2.numpy()
+#        except AttributeError:
+#            pass
+#        r = np.zeros([X1.shape[0],X2.shape[0]])
+#        for i1,x1 in enumerate(X1):
+#            for i2,x2 in enumerate(X2):
+#                    if (x1[1]==x2[1]) and (x1[2]==x2[2]): #if same sensor and component
+#                        r[i1,i2] = self.k(x1[0],x2[0])
+#        return tf.Variable(r,dtype=tf.float32)
         
 
         
@@ -95,8 +162,6 @@ class SparseModel():
         
         self.k = k
         self.Kzz = k.matrix(Z,Z)+np.eye(Z.shape[0],dtype=np.float32)*self.jitter
-        
-        
         self.Kxx = k.matrix(X,X)+np.eye(X.shape[0],dtype=np.float32)*self.jitter
         self.Kxz = k.matrix(X,Z)
         self.Kzx = tf.transpose(self.Kxz)
@@ -172,7 +237,7 @@ def getcov(scale):
     return tf.linalg.band_part(scale, -1, 0) @ tf.transpose(tf.linalg.band_part(scale, -1, 0))
 
 class CalibrationSystem():
-    def __init__(self,X,Y,Z,refsensor,C,transform_fn,gpflowkernel,likemodel='fixed',gpflowkernellike=None,likelihoodstd=1.0,jitter=1e-4,lr=0.02,likelr=None,minibatchsize=100):
+    def __init__(self,X,Y,Z,refsensor,C,transform_fn,gpflowkernels,kernelindices,likemodel='fixed',gpflowkernellike=None,likelihoodstd=1.0,jitter=1e-4,lr=0.02,likelr=None,minibatchsize=100,sideY=None):
         """
         A tool for running the calibration algorithm on a dataset, produces
         estimates of the calibration parameters over time for each sensor.
@@ -198,10 +263,12 @@ class CalibrationSystem():
                                      x number of observations (N) 
                                      x (number of components) (C)].
                  Y's shape is [number of observations (N) x 1].
-        gpflowkernel : A one dimensional kernel that describes all components.
-          TODO: We should allow different kernels for different components.
-                If one wishes to now, then after calling the constructor,
-                set self.k with an alternative kernel object.
+        gpflowkernels = a list of GPflow kernels.
+          E.g. gpflowkernels = [gpflow.kernels.RBF(1.0,200),gpflow.kernels.Bias(1.0)]
+        kernelindices = a list (length C) of lists (number of sensors), 
+         each row refers to the one or more parameters.
+         each element of each list refers to a sensor.
+          E.g. indexes = [[0,0,0,0,1,1]]
                 
         likemodel : specifies how the likelihood is modelled.
         It can be one of four values:
@@ -215,6 +282,8 @@ class CalibrationSystem():
                 a different likelihood.
         jitter : Jitter added to ensure stability (default=1e-4).
         lr, likelr : learning rates.
+        sideY : side information (humidity, temperature, etc. These are provided
+        to the transform_fn)
                              
         TODO what happens if refsensor is integer?
                           if float64 used for others..
@@ -241,8 +310,9 @@ class CalibrationSystem():
         self.likemodel = likemodel
         S = len(refsensor)
         self.C = C
-        self.fullY = Y        
-        self.k = Kernel(gpflowkernel)
+        self.fullsideY = sideY
+        self.fullY = Y
+        self.k = MultiKernel(gpflowkernels,kernelindices)
         
         self.likelihoodstd = likelihoodstd
         self.minibatchsize = minibatchsize
@@ -276,7 +346,9 @@ class CalibrationSystem():
         #definition of q(u)
         M = self.Z.shape[0]
         self.mu = tf.Variable(0.3*tf.random.normal([M,1]))
-        self.scale = tf.Variable(0.05*np.tril(1.0*np.random.randn(M,M)+1.0*np.eye(M)),dtype=tf.float32)        
+        self.scale = tf.Variable(np.tril(0.1*np.random.randn(M,M)+1.0*np.eye(M)),dtype=tf.float32)        
+        #self.mu = tf.Variable(0*tf.random.normal([M,1]))
+        #self.scale = tf.Variable(5*np.eye(M),dtype=tf.float32) #tf.Variable(0.2*np.tril(1.0*np.random.randn(M,M)+1.0*np.eye(M)),dtype=tf.float32)   
         
         if self.likemodel=='distribution' or self.likemodel=='process':
             Mlike = self.Zlike.shape[0]
@@ -297,18 +369,43 @@ class CalibrationSystem():
         self.pu = tfd.MultivariateNormalFullCovariance(mu_u,cov_u+np.eye(cov_u.shape[0])*self.jitter)
         
         
-    def computeforminibatch(self):
-        batch = np.random.choice(self.N,self.minibatchsize,replace=False)
+    def computeforminibatch(self,justuserefs=False):    
+        if justuserefs: #to start with we just train on data from pairings with reference sensors to help optimisation
+            print("!!JUST USING REFERENCE ROWS!!")
+            keep = np.where((np.isin(self.fullX[:,1],np.where(self.refsensor)[0])) | (np.isin(self.fullX[:,2],np.where(self.refsensor)[0])))[0]
+            batch = np.random.choice(keep,self.minibatchsize,replace=False)
+        else:
+            if self.N == self.minibatchsize: #select all of them
+                #print("minibatch is full dataset")
+                batch = np.arange(self.N)
+            else:
+                batch = np.random.choice(self.N,self.minibatchsize,replace=False)
+
         self.X = np.c_[np.tile(np.r_[np.c_[self.fullX[batch,0],self.fullX[batch,1]],np.c_[self.fullX[batch,0],self.fullX[batch,2]]],[self.C,1]),np.repeat(np.arange(self.C),2*self.minibatchsize)] 
         self.Xlike = np.c_[np.r_[np.c_[self.fullX[batch,0],self.fullX[batch,1]],np.c_[self.fullX[batch,0],self.fullX[batch,2]]],np.repeat(0,2*self.minibatchsize)]  
         self.ref = tf.gather(self.refsensor,tf.transpose(tf.reshape(self.X[:(2*self.minibatchsize),1:2].astype(int),[2,self.minibatchsize])))
         self.Y = self.fullY[batch,:]
+        if self.fullsideY is not None:
+            self.sideY = self.fullsideY[batch,:]
+        else:
+            self.sideY = None
         
     def likelihoodfn_nonstationary(self,scaledA,scaledB,varparamA,varparamB):
         return tfd.Normal(0,0.00001+tf.sqrt(tf.exp(varparamA)+tf.exp(varparamB))).log_prob(scaledA-scaledB)    
     
-    def likelihoodfn(self,scaledA,scaledB):
-        return tfd.Normal(0,self.likelihoodstd).log_prob(scaledA-scaledB)
+    #def likelihoodfn(self,scaledA,scaledB):
+    #    return tfd.Normal(0,self.likelihoodstd).log_prob(scaledA-scaledB)
+        
+    def likelihoodfn(self,scaledA,scaledB,ref):
+        likelihoodstd = np.sqrt((self.likelihoodstd**2)*np.sum(1-ref,1))
+        #likelihoodstd = 1e-4+((1-np.any(ref,1))*(self.likelihoodstd)).astype(np.float32)
+        #likelihoodstd = np.sqrt((self.likelihoodstd**2)*np.sum(1-ref,1))
+        #likelihoodstd = self.likelihoodstd/100+self.likelihoodstd*np.min(1-ref,1)
+        #return tfd.Normal(0,0.001+likelihoodstd).log_prob((scaledA-scaledB)/(scaledA+scaledB))
+        #return tfd.Normal(0,self.likelihoodstd).log_prob((scaledA-scaledB)/(scaledA+scaledB))
+        #return tfd.Normal(0,self.likelihoodstd).log_prob(scaledA-scaledB)
+        return tfd.Normal(0,likelihoodstd).log_prob(scaledA-scaledB)
+        
 
     #@tf.function
     def run(self,its=None,samples=100,threshold=0.001):
@@ -328,27 +425,24 @@ class CalibrationSystem():
         print("Starting Run")
         try:
             while (its is None) or (it<its):
+                print(".",end="")                
                 it+=1
                 with tf.GradientTape() as tape:
                     qu = tfd.MultivariateNormalTriL(self.mu[:,0],self.scale)
-                    self.computeforminibatch()
+                    self.computeforminibatch(False) #it<its*0.25) #for first 25% just use reference sensors - this doesn't seem to help anything
                     if self.likemodel=='distribution' or self.likemodel=='process':
                         self.smlike = SparseModel(self.Xlike,self.Zlike,1,self.k)
                     self.sm = SparseModel(self.X,self.Z,self.C,self.k)
-                    
                     samps = self.sm.get_samples(self.mu,self.scale,samples)
-                    scaled = tf.concat([self.transform_fn(samps[:,:,::2],self.Y[:,0:1]),self.transform_fn(samps[:,:,1::2],self.Y[:,1:2])],2)
+                    scaled = tf.concat([self.transform_fn(samps[:,:,::2],self.Y[:,0:1],self.sideY),self.transform_fn(samps[:,:,1::2],self.Y[:,1:2],self.sideY)],2)
                     scaled = (scaled * (1-self.ref)) + (self.Y * self.ref)
-                    
                     if self.mulike is not None: #if we have non-stationary likelihood variance...
                         qulike = tfd.MultivariateNormalTriL(self.mulike[:,0],self.scalelike)              
                         like = self.smlike.get_samples(self.mulike,self.scalelike,samples)
                         ell = tf.reduce_mean(tf.reduce_sum(self.likelihoodfn_nonstationary(scaled[:,:,0],scaled[:,:,1],like[:,:,0]*(1-self.ref[:,0])-1000*self.ref[:,0],like[:,:,1]*(1-self.ref[:,1])-1000*self.ref[:,1]),1))
                     else: #stationary likelihood variance
-                        ell = tf.reduce_mean(tf.reduce_sum(self.likelihoodfn(scaled[:,:,0],scaled[:,:,1]),1))
-                    
+                        ell = tfp.stats.percentile(tf.reduce_sum(self.likelihoodfn(scaled[:,:,0],scaled[:,:,1],self.ref),1), 50.0, interpolation='midpoint')
                     elbo_loss = -((len(self.fullX)*2)/len(self.X) * ell)+tfd.kl_divergence(qu,self.pu)
-                    
                     if self.likemodel=='process':
                         assert self.mulike is not None
                         assert self.scalelike is not None
@@ -356,7 +450,6 @@ class CalibrationSystem():
                     if self.likemodel=='distribution':
                         assert self.mulike is not None
                         elbo_loss -= self.pulike.log_prob(self.mulike[:,0])
-
                     if it%20==0: print("%d (ELBO=%0.4f)" % (it, elbo_loss))
                     
                     if (self.mulike is None) or (it%50<25): #optimise latent fns
@@ -379,6 +472,7 @@ class CalibrationSystem():
                             #check that nothing weird's happened!
                             if np.std(elbo_record[-50:])<np.std(elbo_record[-100:-50]):
                                 break
+                                
         except KeyboardInterrupt:
             pass
         return np.array(elbo_record)
@@ -489,7 +583,7 @@ class SparseModelNoMiniBatch():
         return samps
 
 class CalibrationSystemNoMiniBatch():
-    def __init__(self,X,Y,Z,refsensor,C,transform_fn,gpflowkernel,likemodel='fixed',gpflowkernellike=None,likelihoodstd=1.0,jitter=1e-4,lr=0.02,likelr=None):
+    def __init__(self,X,Y,Z,refsensor,C,transform_fn,gpflowkernel,likemodel='fixed',gpflowkernellike=None,likelihoodstd=1.0,jitter=1e-4,lr=0.02,likelr=None, sideY=None):
         """
         A tool for running the calibration algorithm on a dataset, produces
         estimates of the calibration parameters over time for each sensor.
@@ -532,6 +626,9 @@ class CalibrationSystemNoMiniBatch():
                 a different likelihood.
         jitter : Jitter added to ensure stability (default=1e-4).
         lr, likelr : learning rates.
+        sideY : side information (humidity, temperature, etc. These are provided
+        to the transform_fn)
+        
                              
         TODO what happens if refsensor is integer?
                           if float64 used for others..
@@ -558,7 +655,8 @@ class CalibrationSystemNoMiniBatch():
         self.likemodel = likemodel
         S = len(refsensor)
         self.C = C
-        self.Y = Y        
+        self.Y = Y    
+        self.sideY = sideY    
         self.k = Kernel(gpflowkernel)
         
         self.likelihoodstd = likelihoodstd
@@ -643,7 +741,7 @@ class CalibrationSystemNoMiniBatch():
                 with tf.GradientTape() as tape:
                     qu = tfd.MultivariateNormalTriL(self.mu[:,0],self.scale)
                     samps = self.sm.get_samples(self.mu,self.scale,samples)
-                    scaled = tf.concat([self.transform_fn(samps[:,:,::2],self.Y[:,0:1]),self.transform_fn(samps[:,:,1::2],self.Y[:,1:2])],2)
+                    scaled = tf.concat([self.transform_fn(samps[:,:,::2],self.Y[:,0:1],self.sideY),self.transform_fn(samps[:,:,1::2],self.Y[:,1:2],self.sideY)],2)
                     scaled = (scaled * (1-self.ref)) + (self.Y * self.ref)
                     
                     if self.mulike is not None: #if we have non-stationary likelihood variance...
